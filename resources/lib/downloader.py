ff20e72da77df169d4c2cc6aa40f3cfc3f642221
KZ <kubedzero@gmail.com>
2021-03-13 15:03:13 -0800

    Update xbmc.translatePath to xbmcvfs

--- resources/lib/downloader.py
+++ resources/lib/downloader.py
@@ -66,7 +66,7 @@ class Downloader:
             xbmcvfs.delete(path)
 
         self.dp.update(0, name)
-        self.path = xbmc.translatePath(path)
+        self.path = xbmcvfs.translatePath(path)
         xbmc.sleep(500)
         start_time = time.time()
 
