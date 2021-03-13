3b79a332c51c538125edff374d4fb6700b2b7656
enen92 <92enen@gmail.com>
2021-03-13 15:23:39 +0000

    Update checksum generator script to python3

--- entrychecksumgenerator.py
+++ entrychecksumgenerator.py
@@ -20,22 +20,22 @@
     Note: This is a standalone script to update the offline video entries and
     their checksums
 """
-import urllib2
 import os
 import json
 import hashlib
 import shutil
 import sys
+from urllib import request
 
-applefeed = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
+applefeed = "http://a1.v2.phobos.apple.com.edgesuite.net/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
 applelocalfeed = os.path.join("resources", "entries.json")
 tmpfolder = "tmpvideos"
 
 
 def generate_entries_and_checksums():
     try:
-        req = urllib2.request.Request(applefeed)
-        response = urllib2.urlopen(req)
+        req = request.Request(applefeed)
+        response = request.urlopen(req)
     except Exception:
         print("Failed to open Apple Feed, aborting")
         return
@@ -43,7 +43,7 @@ def generate_entries_and_checksums():
     if response.getcode() == 200:
         html = response.read()
         print("Updating offline local video feed file...")
-        with open(applelocalfeed, "w") as f:
+        with open(applelocalfeed, "wb") as f:
             f.write(html)
         print("Offline feed file updated!")
 
@@ -58,23 +58,23 @@ def generate_entries_and_checksums():
         for block in video_feed:
             for asset in block["assets"]:
                 asset_url = asset["url"]
-                print "Processing video %s..." % (asset_url)
+                print(f"Processing video {asset_url}...")
                 file_name = tmpdownload(asset_url)
                 if file_name:
                     with open(os.path.join(tmpfolder, file_name), "rb") as f:
                         checksum = hashlib.md5(f.read()).hexdigest()
                         checksums[file_name] = checksum
                         os.remove(os.path.join(tmpfolder, file_name))
-                        print "File processed. Checksum= %s" % (checksum)
+                        print(f"File processed. Checksum={checksum}")
                 else:
                     failed.append(asset_url)
 
         shutil.rmtree(tmpfolder)
-        print "Updating checksum file"
+        print("Updating checksum file")
         with open(os.path.join("resources", "checksums.json"), "w") as f:
             f.write(json.dumps(checksums))
-            print "All done"
-        print "Failed items: %s" % (str(failed))
+            print("All done")
+        print(f"Failed items: {failed}")
     else:
         print("Failed to open Apple Feed - Wrong status code, aborting")
 
@@ -95,7 +95,7 @@ def get_locations():
             for asset in block["assets"]:
                 if asset["accessibilityLabel"].lower() not in locations:
                     locations.append(asset["accessibilityLabel"].lower())
-        print locations
+        print(locations)
 
     else:
         print("Failed to open Apple Feed - Wrong status code, aborting")
@@ -103,12 +103,11 @@ def get_locations():
 
 def tmpdownload(url):
     file_name = url.split('/')[-1]
-    req = urllib2.request.Request(url)
-    u = urllib2.urlopen(req)
+    req = request.Request(url)
+    u = request.urlopen(req)
 
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+    file_size = int(u.headers.get("Content-Length"))
+    print(f"Downloading: {file_name} Bytes: {file_size}")
 
     file_size_dl = 0
     block_sz = 8192
@@ -121,7 +120,7 @@ def tmpdownload(url):
             f.write(_buffer)
             status = r"%10d [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
             status = status + chr(8) * (len(status) + 1)
-            print status,
+            print(status)
 
     return False
 
